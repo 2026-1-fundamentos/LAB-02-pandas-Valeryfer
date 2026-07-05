340d1585fa3e666f36e135c9d85ea81e7f8b57e7
Valeryfer <vafernandez@unal.edu.co>
2026-07-05 17:49:42 -0500

    Finished

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_01():
     """
@@ -14,3 +14,5 @@ def pregunta_01():
     40
 
     """
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    return df.shape[0]
--- homework/pregunta_13.py
+++ homework/pregunta_13.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_13():
     """
@@ -20,3 +20,10 @@ def pregunta_13():
     E    275
     Name: c5b, dtype: int64
     """
+
+    tbl0 = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    tbl2 = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+
+    df = pd.merge(tbl0, tbl2, on="c0")
+
+    return df.groupby("c1")["c5b"].sum()
